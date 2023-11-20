97f30110825c79dc2e7d22945a0a17a7f48f2bc9
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-11-19 16:00:46 -0800

    task 7 on state all functions written, locally tested - looks good. Will see what the checker says

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''Handling RESTful API on state'''
-from flask import jsonify
+from flask import jsonify, request
 from api.v1.views import app_views
 from models import storage
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -36,6 +37,35 @@ def delete_id(state_id):
     except:
         return jsonify({'error': 'Error retrieving state'}), 404
 
+
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     '''Creates a state'''
+    try:
+        json_data = request.get_json()
+        if not isinstance(json_data, dict):
+            return jsonify({'error': 'Not a JSON'}), 400
+        if 'name' not in json_data:
+            return jsonify({'error': 'Missing name'}), 400
+        new_state = State(name=json_data['name'])
+        storage.new(new_state)
+        storage.save()
+        return jsonify(new_state.to_dict()), 201
+    except:
+        return jsonify({'error': 'Error creating state'})
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    '''Updates a state object'''
+    state = storage.get('State', str(state_id))
+    if state is None:
+        return jsonify({'error': 'Not Found'}), 404
+    json_data = request.get_json()
+    if not isinstance(json_data, dict):
+        return jsonify({'error': 'Not a JSON'}), 400
+    for key, value in json_data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
